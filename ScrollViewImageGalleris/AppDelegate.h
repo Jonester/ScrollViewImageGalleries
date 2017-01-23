fb2f9987e2c1d5dfe8bf04cf6f67d2bdd50fdde0
Jonester <chris.jones78@gmail.com>
2017-01-23 14:27:47 -0500

    Initial Commit

---
+++ ScrollViewImageGalleris/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGalleris
+//
+//  Created by Chris Jones on 2017-01-23.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
